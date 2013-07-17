fb7b2b00d72ec2cfabfecddc988da89a7ffd7583
Arialdo Martini <arialdomartini@gmail.com>
2013-07-17 14:12:05 +0200

    It can replace agile, agility, kanban and scrum to fluff*

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 agile-to-fluff
 ==============
 
-Got enough of hype about Agile? Replace any reference to Agile with Fluff
+Got enough of hype about Agile? Replace any reference to Agile with Fluff.
+agile-to-fluff is a Chrome extension that replaces every occurrence of words related to Agile  with "fluff".
---
+++ src/content_script.js
@@ -0,0 +1,53 @@
+String.prototype.fuffify = function () {
+    var translations = [
+		['agile', 'fluff'],
+		['Agile', 'Fluff'],
+		['agility', 'fluffity'],
+		['Agility', 'Fluffity'],
+		['AGILITY', 'FLUFFITY'],
+
+		['scrum', 'fluff'],
+		['Scrum', 'Fluff'],
+		['SCRUM', 'FLUFF'],
+
+		['Kanban', 'Fluff'],
+		['kanban', 'fluff'],
+		['KANBAN', 'FLUFF'],
+
+		['Lean', 'Fluff'],
+		['lean', 'fluff'],
+		['LEAN', 'FLUFF']
+	];
+	
+	var v = this;
+	for(var key in translations)
+		v = v.replace(new RegExp( translations[key][0], 'g' ), translations[key][1] );
+    return v;
+};
+
+function walk(node) 
+{
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:
+		case 9:
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			node.nodeValue = node.nodeValue.fuffify();
+			break;
+	}
+}
+
+
+walk(document.body);
